638ff21238f09a9d416b6a79bf94193e8b9fb0d3
ClementBrifault <clement.brifault@gmail.com>
2021-04-09 10:47:12 +0200

    req added

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import time
 from tqdm import tqdm
 
-for i in tqdm(range(3)):
-    time.sleep(6)
+for i in tqdm(range(5*60)):
+    time.sleep(1)
 
 with open('file.txt', 'w') as f:
     f.write('TESTFILE')
