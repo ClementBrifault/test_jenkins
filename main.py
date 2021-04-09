76921056ab8a09564de3be47000db061f8ba14b7
ClementBrifault <clement.brifault@gmail.com>
2021-04-09 13:26:17 +0200

    without cloning

--- main.py
+++ main.py
@@ -5,6 +5,6 @@ for i in tqdm(range(1*60)):
     time.sleep(1)
 
 with open('file.txt', 'w') as f:
-    f.write('TESTFILE')
+    f.write('TESTFILE version sans clone dans le Jenkinsfile')
 
 print('Work done')
