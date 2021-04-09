4da318dbb134adf94c83a10b1f002c5a4f31b8e0
ClementBrifault <clement.brifault@gmail.com>
2021-04-09 10:27:07 +0200

    second

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import time
 from tqdm import tqdm
 
-for i in tqdm(range(100)):
+for i in tqdm(range(3)):
     time.sleep(6)
+
+with open('file.txt', 'w') as f:
+    f.write('TESTFILE')
 
 print('Work done')
