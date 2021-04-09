3c1bd76e9953ea993ea37485d821c33ebee24ddb
ClementBrifault <43877328+ClementBrifault@users.noreply.github.com>
2021-04-09 14:22:52 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import time
 from tqdm import tqdm
 
-for i in tqdm(range(1*60)):
+for i in tqdm(range(2*60)):
     time.sleep(1)
 
 with open('file.txt', 'w') as f:
-    f.write('1 minutes expe')
+    f.write('2 minutes expe')
 
 print('Work done')
